72250ab6b15e48bb1f3fbe4d892e2187dbf8c82c
Bruno Ferrari <kanedaferrari@yahoo.com.br>
2026-08-21 09:40:25 -0300

    nova tentativa de correção do multer

--- src/common/services/cloudnary/cloudnary.service.ts
+++ src/common/services/cloudnary/cloudnary.service.ts
@@ -1,7 +1,7 @@
+import Multer from 'Multer'
 import { Readable } from 'node:stream'
 import { Injectable } from '@nestjs/common'
 import { v2 as cloudinary } from 'cloudinary'
-import 'multer'
 
 @Injectable()
 export class CloudinaryService {
